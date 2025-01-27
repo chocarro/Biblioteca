566ce6e556d99f11cedfdfcd6006b154c55361bd
chocarro <chocarroo@gmail.com>
2025-01-27 16:15:15 +0100

    Cambios finales

--- Tarea 3 tema 5/src/Libros.java
+++ Tarea 3 tema 5/src/Libros.java
@@ -5,11 +5,13 @@ class Libro { // Variables para Libro
     private String titulo;
     private String autor;
     private String categoria;
+    private boolean estaPrestado;
 
-    public void Libro(String titulo, String autor, String categoria) {// Constructor parametrizar
+    public Libro(String titulo, String autor, String categoria, Boolean estaPrestado) {// Constructor parametrizar
         this.titulo = titulo;
         this.autor = autor;
         this.categoria = categoria;
+        this.estaPrestado=estaPrestado;
     }
 
     // Get and Set
@@ -38,41 +40,16 @@ class Libro { // Variables para Libro
         this.categoria = categoria;
     }
 
+    public boolean isEstaPrestado() {
+        return estaPrestado;
+    }
+
+    public void setEstaPrestado(boolean estaPrestado) {
+        this.estaPrestado = estaPrestado;
+    }
     @Override
     public String toString() {
         return "Titulo" + titulo + ", Autor" + autor + ", Categotia" + categoria;
     }
-
-    // Funciones
-    List<Libro> libros = new ArrayList<>();
-
-    public void agregarLibro(String titulo, String autor, String categoria) {
-        libros.add(new Libro());
-        System.out.println("Libro agregado correctamente.");
-    }
-
-    public void eliminarLibro(String titulo) {
-        libros.removeIf(libro -> libro.titulo.equalsIgnoreCase(titulo));
-        System.out.println("Libro eliminado si existía.");
-    }
-
-    public void buscarLibro(String criterio, String valor) {
-        for (Libro libro : libros) {
-            if ((criterio.equalsIgnoreCase("titulo") && libro.titulo.equalsIgnoreCase(valor)) ||
-                    (criterio.equalsIgnoreCase("autor") && libro.autor.equalsIgnoreCase(valor)) ||
-                    (criterio.equalsIgnoreCase("categoria") && libro.categoria.equalsIgnoreCase(valor))) {
-                System.out.println(libro);
-            }
-        }
-    }
-
-    public void mostrarLibros() {
-        if (libros.isEmpty()) {
-            System.out.println("No hay libros disponibles.");
-        } else {
-            for (Libro libro : libros) {
-                System.out.println(libro);
-            }
-        }
-    }
+    
 }
